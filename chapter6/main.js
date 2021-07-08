c360f64e308c7311f4fbc56a7cd231ac27736269
AbdulBasit0044 <abdulbasit0044@gmail.com>
2021-07-09 04:01:46 +0500

    Checking the DOM functions

---
+++ chapter6/main.js
@@ -0,0 +1,10 @@
+let para = document.querySelector('div.error');
+
+console.log(para);
+
+let s ='a';
+let r = 'b';
+let resultS = s.match(/[aeiou]/);
+console.log(resultS);
+let resultR = r.match(/^[aeiou]/);
+console.log(resultR);
