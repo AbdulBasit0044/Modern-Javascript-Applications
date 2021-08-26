c0a02854a0ef4b7434a8325371904975c07d5574
AbdulBasit0044 <abdulbasit0044@gmail.com>
2021-08-27 00:33:19 +0500

    Create strings.js

---
+++ chapter_2/strings.js
@@ -0,0 +1,24 @@
+console.log("Hello World");
+
+let email = "hello@example.com";
+
+console.log(email);
+
+let firstName = "Abdul";
+let lastName = "Basit";
+
+let fullName = firstName + lastName;
+console.log(fullName);
+
+//getting characters
+console.log(fullName[0]);
+
+//getting string length
+console.log(fullName.length);
+
+//string methods
+//converting to the upper case
+console.log(fullName.toUpperCase());
+
+let result = fullName.lowercase();
+console.log(result);
