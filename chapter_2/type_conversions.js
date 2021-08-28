7d85a2c1160b3892ad6ad7e40de52fff8aac9f34
AbdulBasit0044 <abdulbasit0044@gmail.com>
2021-08-29 01:02:48 +0500

    This is amazing

---
+++ chapter_2/type_conversions.js
@@ -0,0 +1,16 @@
+let score = '100';
+
+score =  Number(score);
+console.log(score + 1);
+console.log(typeof(score));
+
+let result = Number("hello");
+console.log(result);
+
+result = Number("50");
+console.log(result, String(result));
+
+let boolValue = Boolean(100);
+console.log(boolValue, typeof(boolValue));
+boolValue = Boolean(0);
+console.log(boolValue, typeof(boolValue));
