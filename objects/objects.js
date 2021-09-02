8a818f5c3044d410fd2ad89c46337016bbdcc042
AbdulBasit0044 <abdulbasit0044@gmail.com>
2021-09-03 04:10:40 +0500

    Update objects.js

--- objects/objects.js
+++ objects/objects.js
@@ -3,13 +3,35 @@ let user = {
     age: 32,
     email: 'geraltofrivia@witcher.com',
     location: 'rivia',
+    blogs: ["Kill the monster 1", 'Kill the monster 2'],
+    login: function(){
+       console.log("user is logged in"); 
+    },
+    logout: function(){
+        console.log("user is logged out");
+    },
+    //###########################################//
+    logBlogs: function(){
+        console.log(this);//this will return the current object if written in regular function
+        console.log(`${this.name} have killed the following monsters`);
+        this.blogs.forEach(element => {
+            console.log(element);
+        });
+    }
+    //###########################################//
+    // logBlogs: ()=>{
+    //     console.log(this);//this will return the global window object if written in arrow function
+    // }
 }
 
-console.log(user);
-console.log(user.name);
-console.log(user['name']);
+// console.log(user);
+// console.log(user.name);
+// console.log(user['name']);
 
-user.name = 'The white wolf';
-console.log(user.name);
-user['name'] = "The monster slayer";
-console.log(user.name);
+// user.name = 'The white wolf';
+// console.log(user.name);
+// user['name'] = "The monster slayer";
+// console.log(user.name);
+
+user.logBlogs();
+console.log(this);//this will return the global window object
